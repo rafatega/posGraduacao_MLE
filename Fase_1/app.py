20154a95255094a7f8efc50b25a119d95ce00e76
RafaTega <rafael_tegazzini@hotmail.com>
2025-12-25 18:37:36 -0300

    Adicionado WebScrapping junto a API do Flask

--- Fase_1/app.py
+++ Fase_1/app.py
@@ -1,8 +1,33 @@
 from flask import Flask, jsonify, request
+import requests
+from flask_httpauth import HTTPBasicAuth
+from flasgger import Swagger
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+app.config['SWAGGER'] = {
+    'title': 'My Flask API',
+    'uiversion': 3
+}
+
+swagger = Swagger(app)
+
 items = []
+
+
+auth = HTTPBasicAuth()
+
+users = {
+    "user1": "password1",
+    "user2": "password2"
+}
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and users[username] == password:
+        return username
 
 
 @app.route('/')
@@ -11,6 +36,7 @@ def home():
 
 
 @app.route('/items', methods=['GET'])
+@auth.login_required
 def get_items():
     return jsonify(items)
 
@@ -37,6 +63,63 @@ def delete_item(item_id):
         removed = items.pop(item_id)
         return jsonify(removed)
     return jsonify({"error": "Item not found"}), 404
+
+
+def get_title(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.title.string.strip()
+        return jsonify({"title": title})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/scrape/content', methods=['GET'])
+@auth.login_required
+def get_content():
+    url = request.args.get('url')
+    if not url:
+        return jsonify({"error": "URL is required"}), 400
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        headers = []
+        for header_tag in ['h1', 'h2', 'h3']:
+            for header in soup.find_all(header_tag):
+                headers.append(header.get_text(strip=True))
+
+        paragraphs = [p.get_text(strip=True) for p in soup.find_all('p')]
+
+        return jsonify({"headers": headers, "paragraphs": paragraphs})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/scrape/title', methods=['GET'])
+@auth.login_required
+def scrape_title():
+    """
+    Extract the title of a web page provided by the URL
+    ---
+    security:
+    - BasicAuth: []
+    parameters:
+     - name: url
+     - in: query
+     - type: string
+     - required: true
+     - description: URL of the page
+    response:
+     200:
+      - description: Web page title
+    """
+    url = request.args.get('url')
+    if not url:
+        return jsonify({"error": "URL is required"}), 400
+    return get_title(url)
 
 
 if __name__ == '__main__':
